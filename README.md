012eaf519818d097b9fa04f3a3bec039c95fd16b
Ivan Fyodor <aamerson198@gmail.com>
2020-11-03 16:03:30 -0500

    adding new readme to check formatting

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
-Na
+# Read Me Maker 
+## Description
+The application is built with the intent to This Application builds read me files .
+## Author 
+This is an application built by Drew Amerson.
 
-This is an application built by drew.
 
-The application is built with the intent to to help .
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contact](#contact)
 
-In order to use this app you must start and finish.
 
-Special credit in the building of this application goes to crystal .
+## License
+This project has a Apache License 2.0 license. 
+## Usage
 
-If you have any additional questions about this application, feel free to contact the application builder here: aamerson @gmail.
+In order to use this app you must Go to Node beging answering questions about your readme/app and it generates a readme.
+## Credit 
+
+Special credit in the building of this application goes to Jonathan Watson .
+
+## Contact 
+
+If you have any additional questions about this application, feel free to contact the application builder here: aamerson@gmail.com.
+
+If you would like to browse the GitHub page of this applications builder visit their Github profile here: https://github.com/aamerson198. 
--- index.js
+++ index.js
@@ -14,6 +14,20 @@ type: "input",
 message: "What is your name?",
 name: "name",
 },
+
+{
+type: "list",
+message: "What type of license would you like?",
+name: "licenseType",
+
+    choices: [
+        "MIT License",
+        "Apache License 2.0",
+        "GNU GPLv3",
+    ]
+
+},
+
 {
     type: "input",
     message: "What is the purpose of your application?",
@@ -35,19 +49,44 @@ name: "name",
                 name: "email",
                 },
 
+                {
+                    type: "input",
+                    message: "what is your github username?",
+                    name: "github",
+
+
+
+                }
+
 ]).then(response => {
     console.log(response);
-    const readMeTemplate = `${response.applicationName}
-
+    const readMeTemplate = `# ${response.applicationName}
+## Description
+The application is built with the intent to ${response.applicationPurpose}.
+## Author 
 This is an application built by ${response.name}.
 
-The application is built with the intent to ${response.applicationPurpose}.
+
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contact](#contact)
+
+
+## License
+This project has a ${response.licenseType} license. 
+## Usage
 
 In order to use this app you must ${response.applicationInstructions}.
+## Credit 
 
 Special credit in the building of this application goes to ${response.applicationCredits}.
 
-If you have any additional questions about this application, feel free to contact the application builder here: ${response.email}.`
+## Contact 
+
+If you have any additional questions about this application, feel free to contact the application builder here: ${response.email}.
+
+If you would like to browse the GitHub page of this applications builder visit their Github profile here: https://github.com/${response.github}. `
 
 fs.writeFile("README.md", readMeTemplate, "utf-8", (err) => {
     if (err)throw err;
